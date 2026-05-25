57ecf268d74f539c687a07659abb19a5d5103c2a
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-05-25 05:10:33 -0500

    i've got a boilerplate recruit menu

--- src/CFOS.UI/Dialog/In Play/SquadRecruitDialog.vb
+++ src/CFOS.UI/Dialog/In Play/SquadRecruitDialog.vb
@@ -1,21 +1,20 @@
 ﻿Imports CFOS.Model
 Imports TGGD.UI
 
-Friend Class SquadMenuDialog
+Friend Class SquadRecruitDialog
     Inherits BaseModelDialog(Of ISquadModel)
+
     Private Sub New(context As IHostContext, model As ISquadModel)
         MyBase.New(context, model)
     End Sub
+
     Friend Shared Function Launch(context As IHostContext, model As ISquadModel) As Func(Of IDialog)
-        Return Function() New SquadMenuDialog(context, model)
+        Return Function() New SquadRecruitDialog(context, model)
     End Function
 
     Public Overrides Function Run() As IDialog
         context.Clear()
-
-        Return context.Choose(
-            "Squad Menu:",
-            DialogChoice.CreateEnabled("Never Mind", Neutral.GetNextDialog(context, model.WorldModel)),
-            DialogChoice.CreateEnabled("Recruit...", SquadRecruitDialog.Launch(context, model)))
+        Return context.Choose("Recruit Whom:",
+            DialogChoice.CreateEnabled("Never Mind", SquadMenuDialog.Launch(context, model)))
     End Function
 End Class
