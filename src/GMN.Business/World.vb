6cf8afedb875f6a3f9915b6a71af96519de0786e
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-05-30 05:55:49 -0500

    added gmn business

---
+++ src/GMN.Business/World.vb
@@ -0,0 +1,16 @@
+﻿Imports GMN.Data
+Imports TGGD.Business
+
+Public Class World
+    Inherits Entity(Of GMNData)
+    Implements IWorld
+    Private Sub New(data As GMNData)
+        Me.EntityData = data
+    End Sub
+
+    Protected Overrides ReadOnly Property EntityData As GMNData
+
+    Public Shared Function Create(data As GMNData) As IWorld
+        Return New World(data)
+    End Function
+End Class
